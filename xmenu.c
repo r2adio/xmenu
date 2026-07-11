1636ae8adacd00f4214405634396b422c427c4c6
gauravashisth <miya.musashi.moto1645@gmail.com>
2026-07-11 19:02:32 +0530

    feat: prints std-input values (dummy data) as xmenu choices

--- xmenu.c
+++ xmenu.c
@@ -7,6 +7,11 @@
 
 #define MAX_BUF 30
 
+char **get_inputs() {
+  static char *dummy_data[] = {"ghostty", "kitty", "gimp", NULL};
+  return dummy_data;
+}
+
 int main() {
   if (tb_init() != 0) return EXIT_FAILURE;
 
@@ -18,8 +23,19 @@ int main() {
     // NOTE: this loop set colors before user starts typing
     // for (int i = 0; i < MAX_BUF; i++)
     //   tb_set_cell(i, 0, ' ', TB_BLUE, TB_DEFAULT);
-    for (int i = 0; i < len; i++)
+    for (int i = 0; i < len; i++) // input box
       tb_set_cell(i, 0, buf[i], TB_BLUE | TB_BOLD, TB_DEFAULT);
+
+    int x = MAX_BUF + 1;
+    char **inputs = get_inputs();
+    for (int i = 0; inputs[i] != NULL; i++) { // prints std inputs
+      if (i > 0) {
+        tb_printf(x, 0, TB_BLACK, TB_WHITE, "|");
+        x += 1;
+      }
+      tb_printf(x, 0, TB_BLACK, TB_WHITE, " %s ", inputs[i]);
+      x += strlen(inputs[i]) + 2;
+    }
     tb_set_cursor(len, 0);
     tb_present();
 
@@ -27,10 +43,7 @@ int main() {
     tb_poll_event(&ev);
 
     if (ev.type == TB_EVENT_KEY) {
-      if (ev.key == TB_KEY_ESC || ev.key == TB_KEY_ENTER) {
-        printf("print: %s", buf);
-        break;
-      }
+      if (ev.key == TB_KEY_ESC || ev.key == TB_KEY_ENTER) break;
       if (ev.key == TB_KEY_BACKSPACE || ev.key == TB_KEY_BACKSPACE2 ||
           ev.key == TB_KEY_CTRL_H)
       {
@@ -43,5 +56,6 @@ int main() {
   }
 
   tb_shutdown();
+  printf("print: %s\n", buf);
   return EXIT_SUCCESS;
 }
